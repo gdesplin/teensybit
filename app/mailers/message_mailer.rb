f1364facf0fa54a86c1239f6f3795388dbe22e4f
Gavin Esplin <gdesplin@gmail.com>
2022-04-06 13:26:48 -0600

    Adds sidekiq worker & procfile And url to production db config

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,9 +2,8 @@ class MessageMailer < ApplicationMailer
 
   def email_message
     @message = Message.find(params[:message_id])
-    puts "getting ready to email message #{@message.message_body}"
     return if @message.recipient_read_at.present?
-    puts "mailing #{@message.message_body}"
+
     mail(to: params[:email], subject: "You have an unread message from #{@message.user.name} at Teensy Bit")
   end
 
